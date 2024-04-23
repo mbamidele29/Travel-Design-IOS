b4eef63853362e5fd64306fe57a76dba34869a6c
Michael Bamidele <mbamidele29@gmail.com>
2024-04-23 19:52:33 +0100

    added launch screen

---
+++ Travel-Design/Utils/Assets.swift
@@ -0,0 +1,26 @@
+//
+//  Assets.swift
+//  Travel-Design
+//
+//  Created by PC on 23/04/2024.
+//
+
+import Foundation
+
+
+struct Assets {
+    static let archive = "archive"
+    static let arrowLeft = "arrow_left"
+    static let heart = "heart"
+    static let marker = "marker"
+    static let profile = "profile"
+    static let setting = "setting"
+    static let star = "star"
+    static let fuji = "fuji"
+    static let andes = "andes"
+    static let clock = "clock"
+    static let send = "send"
+    static let cloud = "cloud"
+    static let starFilled = "star_filled"
+    
+}
---
+++ Travel-Design/Utils/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Travel-Design
+//
+//  Created by PC on 23/04/2024.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    static let __888888 = Color("888888")
+    static let _C5C5C5 = Color("C5C5C5")
+    static let _D2D2D2 = Color("D2D2D2")
+    static let _2F2F2F = Color("2F2F2F")
+    static let _FBFBFB = Color("FBFBFB")
+    static let _1D1D1D = Color("1D1D1D")
+    static let _CAC8C8 = Color("CAC8C8")
+    static let _1B1B1B = Color("1B1B1B")
+    static let _7E7E7E = Color("7E7E7E")
+    static let _A5A5A5 = Color("A5A5A5")
+    static let _EDEDED = Color("EDEDED")
+}
+
+
